5090072283ea1e12482eb608d3840788a93fd000
sanyueyuxincao <lm_hang@163.com>
2017-05-26 10:12:49 +0800

    常规修改

---
+++ python36/text_deal/test01.md
@@ -0,0 +1,29 @@
+#b 白平衡——照片色调的奥秘
+#p 赤橙黄绿青蓝紫，我们看到的光线全是由这些颜色混合而成的。不同的环境、时间下的光线，颜色有很大的差异，也会直接反应在照片上，改变着照片的色调。那么如何控制光线对色调的影响呢？答案就是白平衡。
+#i http://www.nikon.com.cn/microsite/beginnertutor/common/images/base_knowledge/white_balance/wb_img_01.jpg[]白平衡图片一
+#b 自动白平衡（AWB）
+#i http://www.nikon.com.cn/microsite/beginnertutor/common/images/base_knowledge/white_balance/wb_img_02.jpg[]白平衡图片二
+#i http://www.nikon.com.cn/microsite/beginnertutor/common/images/base_knowledge/white_balance/wb_img_03.jpg[]白平衡图片三
+#p 在绝大多数情况下，我们可以放心地使用自动白平衡模式，在这种模式下相机会根据计算自动给出合适的白平衡设置。但是在一些光线复杂的环境下，或者你想营造不一样的拍摄效果，就需要选择与场景或者创意相对应的白平衡了。尼康相机中预设了白炽灯、荧光灯、晴天、闪光灯、阴天以及背阴六种白平衡选项。
+#i http://www.nikon.com.cn/microsite/beginnertutor/common/images/base_knowledge/white_balance/wb_ex_img_01.jpg[]例图一
+#ci 白炽灯
+#i http://www.nikon.com.cn/microsite/beginnertutor/common/images/base_knowledge/white_balance/wb_ex_img_02.jpg[]例图二
+#ci 荧光灯
+#i http://www.nikon.com.cn/microsite/beginnertutor/common/images/base_knowledge/white_balance/wb_ex_img_03.jpg[]例图三
+#ci 晴天
+#i http://www.nikon.com.cn/microsite/beginnertutor/common/images/base_knowledge/white_balance/wb_ex_img_04.jpg[]例图四
+#ci 闪光灯
+#i http://www.nikon.com.cn/microsite/beginnertutor/common/images/base_knowledge/white_balance/wb_ex_img_05.jpg[]例图五
+#ci 阴天
+#i http://www.nikon.com.cn/microsite/beginnertutor/common/images/base_knowledge/white_balance/wb_ex_img_06.jpg[]例图六
+#ci 背阴
+#b 手动白平衡
+#i http://www.nikon.com.cn/microsite/beginnertutor/common/images/base_knowledge/white_balance/wb_img_04.jpg[]白平衡图片四
+#i http://www.nikon.com.cn/microsite/beginnertutor/common/images/base_knowledge/white_balance/wb_img_05.jpg[]白平衡图片五
+#p 当光线环境很复杂、使用自动白平衡或者预设白平衡都无法校正照片色偏的时候，部分相机还提供了手动调节色温的选项。使用这种模式，你可以通过色温值更加直接地来调节照片的色调。
+#i http://www.nikon.com.cn/microsite/beginnertutor/common/images/base_knowledge/white_balance/wb_img_06.jpg[]白平衡图片六
+#i http://www.nikon.com.cn/microsite/beginnertutor/common/images/base_knowledge/white_balance/wb_img_07.jpg[]白平衡图片七
+#p 手动预设白平衡是让你可以自定义一种新的预设白平衡的模式。使用该模式一般需要先让相机拍摄一张白纸或者灰卡纸来设置。手动预设模式的操作比较复杂，专业摄影师用得比较多。
+
+	
+				
---
+++ python36/text_deal/text01.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+
+class TextAnalysis:
+	"文本解析类"
+	def __init__(self, content):
+		super(TextAnalysis, self).__init__()
+		self.content = content
+	def deal(self):
+		# 查找顺序1.一级标题，2.二级标题，3.三级标题，4.加粗文本，5.普通文本，6.居中图片 7.居中加粗文字 8.居中图片下方解释文字
+		pattern_title01 = re.compile(r'#\s')    # 一级标题匹配:'# '
+		pattern_title02 = re.compile(r'##\s')   # 二级标题匹配:'## '
+		pattern_title03 = re.compile(r'###\s')  # 三级标题匹配:'### '
+		pattern_contentB = re.compile(r'#b\s')  # 加粗文本:'#b '
+		pattern_contentCustom = re.compile(r'#p\s') #普通文本: '#p '
+		pattern_imgCenter = re.compile(r'#i\s')   #居中图片: '#i '
+		pattern_textCenter = re.compile(r'#c\s')  #居中加粗文字: '#c '
+		pattern_textImgC = re.compile(r'#ci\s')   #居中图片解释: '#ci '
+
+		m_title01 = pattern_title01.match(self.content)
+		m_title02 = pattern_title02.match(self.content)  
+		m_title03 = pattern_title03.match(self.content)  
+		m_contentB = pattern_contentB.match(self.content)
+		m_contentCustom= pattern_contentCustom.match(self.content)
+		m_imgCenter = pattern_imgCenter.match(self.content)
+		m_textCenter = pattern_textCenter.match(self.content)
+		m_textImgC = pattern_textImgC.match(self.content)
+
+		if m_title01:
+			content_deal = self.content[2:]
+			content_final = '<div class="title_01">' + content_deal + '</div>'
+			return content_final
+
+		if m_title02:
+			content_deal = self.content[3:]
+			content_final = '<div class="title_02">' + content_deal + '</div>'
+			return content_final
+
+		if m_title03:
+			content_deal = self.content[4:]
+			content_final = '<div class="title_03">' + content_deal + '</div>'
+			return content_final
+
+		if m_contentB:
+			content_deal = self.content[3:]
+			content_final = '<div class="content_bold">' + content_deal + '</div>'
+			return content_final
+
+		if m_contentCustom:
+			content_deal = self.content[3:]
+			content_final = '<div class="content_custom">' + content_deal + '</div>'
+			return content_final
+
+		if m_imgCenter:
+			s = self.content
+			array = s.split('[]')
+			img = '<img class="center_img" src="%s" alt="%s">' % (array[0][3:],array[1])
+			return img	
+
+		if m_textCenter:
+			content_deal = self.content[3:]
+			content_final = '<div class="content_center">' + content_deal + '</div>'
+			return content_final
+
+		if m_textImgC:
+			content_deal = self.content[4:]
+			content_final = '<div class="content_img_center">' + content_deal + '</div>'
+			return content_final
+
+
+file = open("test01.md")
+html_head_content = open("temp.html")
+while 1:
+    line = file.readline()
+    if not line:
+        break
+    else:
+    	Text = TextAnalysis(line)
+    	content_write = Text.deal()
+    	if content_write is not None:
+    		currentDir = os.getcwd() + '/' + 'pages' + '/' + 'test'
+    		if not os.path.exists(currentDir):
+    			os.makedirs(currentDir)
+    		html_head = html_head_content.read();
+    		# html_head = r'<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>xxxxxx</title><style type="text/css">div.title_01 {font-size:32px;}div.title_02 {font-size:28px;}div.title_03 {font-size:22px; background-color: red;}div.content_bold {margin-bottom: 10px;font-size: 18px;font-weight: bold;}div.content_custom {}div.center_img {width:100%}</style></head><body>'
+    		html_foot = r'</body></html>'
+    		f = open(currentDir + '/' + 'test.html','a')
+    		f.write(html_head)
+    		f.write(content_write)
+    		f.write(html_foot)
+f.close()
+
+
+
+
+
+    
